a020645d9ba47a7f6a2896ec7747f0798dad3f7b
Matt Basile <matthewbasile2@gmail.com>
2019-02-11 16:36:41 -0500

    All CRUD calls in place, refactoring to align endpoints specs

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const users = require('./data/db.js')
 
-// creates an express application using the express module
 const server = express();
+server.use(express.json());
+
+// creates an express application using the express module
+
 //Console.log on Home route
 server.get('/', (req, res) => {
   // express will pass the request and response objects to this function
@@ -34,20 +37,31 @@ server.get('/api/users/:id', (req, res) => {
     res.status(code).json({success: false, message})
 })
 });
-server.post('/api/users', (req, res) =>{
-  const user = req.body; 
-  console.log(user)
-  users
-  .insert(user)
-  .then(id =>{
-    if(id) {
-        res.status(200).json({success: true, id});
-    } else{
-        res.status(404).json({success:false, message: 'These are not the hubs you\'re looking for'});
-    }
-})
-})
-
+//ADD USER
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  const newUser = { name, bio };
+  if (!name || !bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  users.insert(newUser)
+    .then(userId => {
+      const { id } = userId;
+      users.findById(id).then(user => {
+        console.log(user);
+        if (!user) {
+          return res
+            .status(422)
+            .send({ Error: `User does not exist by that id ${id}` });
+        }
+        res.status(201).json(user);
+      });
+    })
+    .catch(err => console.error(err));
+});
+//DELETE USER
 server.delete('/api/users/:id', (req, res) => {
   const id = req.params.id;
   users
@@ -63,7 +77,22 @@ server.delete('/api/users/:id', (req, res) => {
       res.status(code).json({success: false, message})
   })
 });
-
+//Edit USER
+server.put('/api/users/:id', (req,res)=>{
+  const {id} = req.params;
+  const changes = req.body;
+  users.update(id, changes)
+  .then(updated =>{
+      if(updated) {
+          res.status(201).json({success: true, updated});
+      } else{
+          res.status(404).json({success:false, message: 'Sorry no Hobbit exists with that ID'});
+      }
+  })
+  .catch(({code, message}) =>{
+      res.status(code).json({success: false, message})
+  })
+})
 
 
 
@@ -79,6 +108,4 @@ server.listen(8000, () => console.log('API running on port 8000'));
 
 
 
-// configures our server to execute a function for every GET request to "/"
-// // the second argument passed to the .get() method is the "Route Handler Function"
-// // the route handler function will run on every GET request to "/"
+
