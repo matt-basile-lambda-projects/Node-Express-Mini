5495451c7f26896308bf3faa3c1647ef5a4dcdea
Matt Basile <matthewbasile2@gmail.com>
2019-02-11 15:44:00 -0500

    Delete functionality included

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const users = require('./data/db.js')
 
 // creates an express application using the express module
 const server = express();
-
+//Console.log on Home route
+server.get('/', (req, res) => {
+  // express will pass the request and response objects to this function
+  // the .send() on the response object can be used to send a response to the client
+  res.send('Hello World');
+});
 // GET ALL USERS - R in CRUD
-server.get('/users', (req, res) => {
+server.get('/api/users', (req, res) => {
   users
   .find()
   .then(users => {
@@ -18,8 +23,7 @@ server.get('/users', (req, res) => {
 })
 });
 //GET A USER - R in CRUD
-
-server.get('/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
   const {id} = req.params;
   users
   .findById(id)
@@ -30,6 +34,41 @@ server.get('/users/:id', (req, res) => {
     res.status(code).json({success: false, message})
 })
 });
+server.post('/api/users', (req, res) =>{
+  const user = req.body; 
+  console.log(user)
+  users
+  .insert(user)
+  .then(id =>{
+    if(id) {
+        res.status(200).json({success: true, id});
+    } else{
+        res.status(404).json({success:false, message: 'These are not the hubs you\'re looking for'});
+    }
+})
+})
+
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  users
+  .remove(id)
+  .then(updated =>{
+    if(updated) {
+        res.status(204).end();
+    } else{
+        res.status(404).json({success:false, message: 'These are not the hubs you\'re looking for'});
+    }
+  })
+    .catch(({code, message}) =>{
+      res.status(code).json({success: false, message})
+  })
+});
+
+
+
+
+
+
 
 
 // once the server is fully configured we can have it "listen" for connections on a particular "port"
@@ -43,8 +82,3 @@ server.listen(8000, () => console.log('API running on port 8000'));
 // configures our server to execute a function for every GET request to "/"
 // // the second argument passed to the .get() method is the "Route Handler Function"
 // // the route handler function will run on every GET request to "/"
-// server.get('/', (req, res) => {
-//   // express will pass the request and response objects to this function
-//   // the .send() on the response object can be used to send a response to the client
-//   res.send('Hello World');
-// });
